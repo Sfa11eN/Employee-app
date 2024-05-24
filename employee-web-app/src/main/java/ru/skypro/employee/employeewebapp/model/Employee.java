0175d461f6bb927b1c637d1fb36dae64fcb48108
Sfa11eN <sfallenn@gmail.com>
2024-05-24 16:27:09 +0300

    Final commit with maps

--- employee-web-app/src/main/java/ru/skypro/employee/employeewebapp/model/Employee.java
+++ employee-web-app/src/main/java/ru/skypro/employee/employeewebapp/model/Employee.java
@@ -12,19 +12,27 @@ public class Employee {
     }
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 
     @Override
--- employee-web-app/src/main/java/ru/skypro/employee/employeewebapp/service/impl/EmployeeServiceImpl.java
+++ employee-web-app/src/main/java/ru/skypro/employee/employeewebapp/service/impl/EmployeeServiceImpl.java
@@ -7,15 +7,13 @@ import ru.skypro.employee.employeewebapp.exceptions.EmployeeStorageIsFullExcepti
 import ru.skypro.employee.employeewebapp.model.Employee;
 import ru.skypro.employee.employeewebapp.service.EmployeeService;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 
 public class EmployeeServiceImpl implements EmployeeService {
     private static final int EMPLOYEE_STORAGE_SIZE = 10;
-    private List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     @Override
     public Employee add(String firstName, String lastName) {
@@ -24,35 +22,35 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         }
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
+        employees.put(employee.getFullName(), employee);
         return employee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee.getFullName());
+        return employee;
+    }
+
+    @Override
+    public Employee find(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeNotFoundException();
         }
         employees.remove(employee);
-            return employee;
-        }
-
-        @Override
-        public Employee find (String firstName, String lastName){
-            Employee employee = new Employee(firstName, lastName);
-            if (employees.contains(employee)) {
-                throw new EmployeeNotFoundException();
-            }
-            employees.remove(employee);
-            return employee;
-        }
-
-        @Override
-        public Collection<Employee> findAll () {
-            return employees;
-        }
+        return employee;
     }
+
+    @Override
+    public Collection<Employee> findAll() {
+        return employees.values();
+    }
+}
